f4ddda50d007630f79ffd4ea18f769dc4de7b2a1
biostunt <nice.nikita99@mail.ru>
2021-01-09 22:58:59 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { Query } from 'mysql';
 import { MysqlConnector } from './mysql'
+import {PgConnector} from './postgresql';
 const mysql = new MysqlConnector();
-
+const postgre = new PgConnector();
 
 async function main() {
     await mysql.connect();
-    const res = await mysql.execute("select * from profession")
-    console.log(res);
+    await postgre.connect();
+    const res1 = await mysql.execute("select 'hello world' as just_row");
+    const res2 = await postgre.execute("select 'hello world' as just_row");
+    console.log(res1, res2);
 }
 
 main();
